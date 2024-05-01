6fead8497755b418c3317c55268fb7008691f590
Mireya Avila <mireyaavila147@yahoo.com>
2024-05-01 13:37:00 -0400

    finsihed annuity cal

--- scripts/annuity_cal.js
+++ scripts/annuity_cal.js
@@ -7,7 +7,7 @@ const amountYearsInput = document.getElementById("amountYearsInput");
 const calculateBtn =  document.getElementById("calculateBtn");
 const resetBtn = document.getElementById("resetBtn");
 
-const presentValueOutut = document.getElementById("presentValueOutput";
+const presentValueOutut = document.getElementById("presentValueOutput");
 
 window.onload = init;
 
@@ -18,13 +18,26 @@ function init() {
 }
 
 function onCalculateBtnClicked() {
-    
+    let monthlypayout = Number(monthlyPayoutInput.value);
+    let interestRate = Number(interestRateInput.value) / 100; 
+    let amountYears = Number(amountYearsInput.value);
+    let monthlyInterest = interestRate / 12;
+    let amountMonths = amountYears * 12
+
+    let presentValue = monthlypayout *((1-(1 + monthlyInterest)**-amountMonths) / monthlyInterest);
+
+    presentValueOutut.value = presentValue.toFixed(2);
+
+
 
 
 }
 
 
 function onResetBtnClicked() {
-
+    monthlyPayoutInput.value = "";
+    interestRateInput.value = "";
+    amountYearsInput.value = "";
+    presentValueOutut.value = "";
 
 }
