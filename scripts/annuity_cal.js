f9a38bdb1faca3292b0892ffd65135047384350a
Mireya Avila <mireyaavila147@yahoo.com>
2024-05-01 10:28:30 -0400

    started on annuity calculator

---
+++ scripts/annuity_cal.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const monthlyPayoutInput = document.getElementById("monthlyPayoutInput");
+const interestRateInput = document.getElementById("interestRateInput");
+const amountYearsInput = document.getElementById("amountYearsInput");
+
+const calculateBtn =  document.getElementById("calculateBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+const presentValueOutut = document.getElementById("presentValueOutput";
+
+window.onload = init;
+
+function init() {
+    calculateBtn.onclick = onCalculateBtnClicked;
+    resetBtn.onclick = onResetBtnClicked;
+
+}
+
+function onCalculateBtnClicked() {
+    
+
+
+}
+
+
+function onResetBtnClicked() {
+
+
+}
